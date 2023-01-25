cace09a64f4636ecefe3af487fb6231820b0edc1
nurinamani <nurinamani00@gmail.com>
2023-01-25 13:20:12 +0800

    User management

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/redirects',[HomeController::class,"index"]);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::get('/coordinator', [HomeController::class, 'index'])->name('coordinator');
+Route::get('/lecturer', [HomeController::class, 'index'])->name('lecturer');
+Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
+
+Route::get('/userList', [HomeController::class, 'userList'])->name('userList');
