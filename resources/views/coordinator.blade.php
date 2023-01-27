eb3979a441e670088d02ab726a17a0e4351b52b8
NuraqilaR <106007336+NuraqilaR@users.noreply.github.com>
2023-01-27 09:42:53 +0800

    add navigation to evaluation

--- resources/views/coordinator.blade.php
+++ resources/views/coordinator.blade.php
@@ -12,4 +12,18 @@
             </div>
         </div>
     </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div style="float:left">
+                        <td><h4>Evaluation</h4></td>
+                    </div>
+                    <div style="float:right">
+                        <td><a href="CoordinatorEvaluationMainpage" class="btn btn-primary">Open</a></td>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
